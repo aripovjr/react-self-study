53bea0a514bc044d28047e9bbc88b8dc0cd1c243
Jasurbek Aripov <jasurbek_aripov@mail.ru>
2023-01-08 10:21:31 +0100

    rendering multiple components with keys

--- src/App.js
+++ src/App.js
@@ -1,47 +1,51 @@
-import './App.css';
-import Expense from './components/Expense';
-import './components/expenseStyle.css'
-import NewExpense from './components/NewExpense';
+import { useState } from "react";
+import "./App.css";
+import Expense from "./components/Expense";
+import "./components/expenseStyle.css";
+import NewExpense from "./components/NewExpense";
+
+const INITIAL_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: "e2",
+    title: "New TV",
+    amount: 499.79,
+    date: new Date(2021, 2, 12),
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.12,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
 function App() {
-  const expenses = [
-    {
-      id:'e1',
-      title:"Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14)
-    },
-    {
-      id:'e2',
-      title:"New TV",
-      amount: 499.79,
-      date: new Date(2021, 2, 12)
-    },
-    {
-      id:'e3',
-      title:"Car Insurance",
-      amount: 294.12,
-      date: new Date(2021, 2, 28)
-    },
-    {
-      id:'e4',
-      title:"New Desk",
-      amount: 450,
-      date: new Date(2021, 5, 12)
-    }
-  ]
+  const [expenses, setExpenses] = useState(INITIAL_EXPENSES);
 
-  const addExpenseHandler = expense => { 
-    console.log(expense)
-  }
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...expenses];
+    });
+  };
 
-  return(
-    <div className='app'>
-      <NewExpense onAddExpense={addExpenseHandler}/>
-      <Expense items={expenses}/>
-
+  return (
+    <div className="app">
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expense items={expenses} />
     </div>
-  )
+  );
 }
 
 export default App;
